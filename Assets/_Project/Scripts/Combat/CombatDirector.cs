162a31614f72458fbf30ea7a2683084d3b30343f
Ozan Gözlüklü <ozan.gozluklu@hotmail.com>
2026-03-09 10:05:03 +0300

    Update combatdirector.cs

--- Assets/_Project/Scripts/Combat/CombatDirector.cs
+++ Assets/_Project/Scripts/Combat/CombatDirector.cs
@@ -2,15 +2,11 @@ using UnityEngine;
 
 public class CombatDirector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private SwipeDirection lastReceivedDirection = SwipeDirection.None;
 
-    // Update is called once per frame
-    void Update()
+    public void OnSwipeDirectionCommitted(SwipeDirection dir)
     {
-        
+        lastReceivedDirection = dir;
+        Debug.Log($"CombatDirector received swipe: {dir}");
     }
 }
